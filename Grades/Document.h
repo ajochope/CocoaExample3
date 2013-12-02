0bd02f0411e1caaebae4a1b9e4e0f3bd38dd5f4b
Oscar Calles <ocalles@gmail.com>
2013-12-02 12:29:20 +0100

    comment  L23, L24, L25 - Drag and Drop
    Cocoa Programming L23, L24, L25 - Drag and Drop

--- Grades/Document.h
+++ Grades/Document.h
@@ -4,7 +4,7 @@
 //
 //  Created by Oscar Calles on 11/29/13.
 //  Copyright (c) 2013 ocalles@gmail.com. All rights reserved.
-//
+// Cocoa Programming L23, L24, L25 - Drag and Drop
 
 #import <Cocoa/Cocoa.h>
 
